1ef66a133f0aec461e6c93d4a39388d56dd5a30d
Kevin Thorne <kevint@thedigitalnative.space>
2017-01-27 00:26:13 -0700

    Trying to cater to php 5...single element arrays are treated as the object

--- resources/views/games.blade.php
+++ resources/views/games.blade.php
@@ -16,17 +16,30 @@
                     <th>Start Time</th>
                     <th></th>
                 </tr>
-                @foreach ($live as $liveGame)
+                @if(method_exists($live, 'isEmpty'))
+                    @foreach ($live as $liveGame)
+                        <tr>
+                            <td>{{$liveGame->id}}</td>
+                            <td>{{$liveGame->owner->name}}</td>
+                            <td>{{$liveGame->time_start}}</td>
+                            <td><a class="btn btn-primary"
+                                   href="aoe2hdspectator://downstream/{{$liveGame->id}}/{{$liveGame->owner->id}}"
+                                   role="button">Watch</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                @else
                     <tr>
-                        <td>{{$liveGame->id}}</td>
-                        <td>{{$liveGame->owner->name}}</td>
-                        <td>{{$liveGame->time_start}}</td>
+                        <td>{{$live->id}}</td>
+                        <td>{{$live->owner->name}}</td>
+                        <td>{{$live->time_start}}</td>
                         <td><a class="btn btn-primary"
-                               href="aoe2hdspectator://downstream/{{$liveGame->id}}/{{$liveGame->owner->id}}"
+                               href="aoe2hdspectator://downstream/{{$live->id}}/{{$live->owner->id}}"
                                role="button">Watch</a>
                         </td>
                     </tr>
-                @endforeach
+                @endif
+
             </table>
         @endif
     </div>
@@ -45,18 +58,31 @@
                     <th>Duration</th>
                     <th></th>
                 </tr>
-                @foreach ($finished as $game)
+                @if(method_exists($finished, 'isEmpty'))
+                    @foreach ($finished as $game)
+                        <tr>
+                            <td>{{$game->owner->name}}</td>
+                            <td>{{$game->time_start}}</td>
+                            <td>{{$game->time_end}}</td>
+                            <td>{{($game->time_end - $game->time_start)}}</td>
+                            <td><a class="btn btn-primary"
+                                   href="aoe2hdspectator://downstream/{{$game->id}}/{{$game->owner->id}}"
+                                   role="button">Watch</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                @else
                     <tr>
-                        <td>{{$game->owner->name}}</td>
-                        <td>{{$game->time_start}}</td>
-                        <td>{{$game->time_end}}</td>
-                        <td>{{($game->time_end - $game->time_start)}}</td>
+                        <td>{{$finished->owner->name}}</td>
+                        <td>{{$finished->time_start}}</td>
+                        <td>{{$finished->time_end}}</td>
+                        <td>{{($finished->time_end - $finished->time_start)}}</td>
                         <td><a class="btn btn-primary"
-                               href="aoe2hdspectator://downstream/{{$game->id}}/{{$game->owner->id}}"
+                               href="aoe2hdspectator://downstream/{{$finished->id}}/{{$finished->owner->id}}"
                                role="button">Watch</a>
                         </td>
                     </tr>
-                @endforeach
+                @endif
             </table>
         @endif
     </div>
