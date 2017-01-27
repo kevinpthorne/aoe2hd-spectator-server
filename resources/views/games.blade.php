953cb8eee425c6689013ef59ad72d92c874746a4
Kevin Thorne <kevint@thedigitalnative.space>
2017-01-27 01:00:56 -0700

    Made the game page less ugly

--- resources/views/games.blade.php
+++ resources/views/games.blade.php
@@ -5,11 +5,11 @@
 @section('content')
     <h1>Games</h1>
 
-    <div class="well well-lg">
+    <div class="row well well-lg">
         @if($live === false) @elseif($live->isEmpty())
             <h6>No live games!</h6>
         @else
-            <table>
+            <table class="table">
                 <tr>
                     <th>Game ID</th>
                     <th>Player</th>
@@ -21,7 +21,7 @@
                         <td>{{$liveGame->id}}</td>
                         <td>{{$liveGame->owner->name}}</td>
                         <td>{{$liveGame->time_start}}</td>
-                        <td><a class="btn btn-primary"
+                        <td><a class="btn btn-raised btn-primary"
                                href="aoe2hdspectator://downstream/{{$liveGame->id}}/{{$liveGame->owner->id}}"
                                role="button">Watch</a>
                         </td>
@@ -34,11 +34,11 @@
 
     <h3>Previous Games</h3>
 
-    <div class="well well-lg">
+    <div class="row well well-lg">
         @if($finished === false) @elseif($finished->isEmpty())
             <h6>No games!</h6>
         @else
-            <table>
+            <table class="table">
                 <tr>
                     <th>Player</th>
                     <th>Start Time</th>
@@ -52,7 +52,7 @@
                         <td>{{$game->time_start}}</td>
                         <td>{{$game->time_end}}</td>
                         <td>{{($game->time_end - $game->time_start)}}</td>
-                        <td><a class="btn btn-primary"
+                        <td><a class="btn btn-raised btn-primary"
                                href="aoe2hdspectator://downstream/{{$game->id}}/{{$game->owner->id}}"
                                role="button">Watch</a>
                         </td>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
     <!-- Bootstrap Material Design -->
     <link rel="stylesheet" type="text/css" href="css/bootstrap-material-design.min.css">
     <link rel="stylesheet" type="text/css" href="css/ripples.min.css">
+    <link rel="stylesheet" type="text/css" href="css/app.css">
 
     @php
         require('../app/steamauth/steamauth.php');
