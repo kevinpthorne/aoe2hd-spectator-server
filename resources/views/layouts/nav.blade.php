a2be8e32568eb61ac5c2166494d49ce231e6f2b3
Kevin Thorne <kevint@thedigitalnative.space>
2017-01-28 11:54:00 -0700

    Fixed more refs in nav

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -17,7 +17,7 @@
                 <ul class="nav navbar-nav navbar-right">
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                           aria-expanded="false">{{session('personaname')}} <span class="caret"></span></a>
+                           aria-expanded="false">{{session('steam_personaname')}} <span class="caret"></span></a>
                         <ul class="dropdown-menu">
                             <li><a href="/user/{{session('steamid')}}">Profile</a></li>
                             <li><a href="/you">Account</a></li>
