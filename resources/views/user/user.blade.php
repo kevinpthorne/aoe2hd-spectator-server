231acc0e5b01a4f8b0c83af06b09e248fb416229
Kevin Thorne <kevint@thedigitalnative.space>
2017-01-28 12:13:23 -0700

    Made user page pretty

--- resources/views/user/user.blade.php
+++ resources/views/user/user.blade.php
@@ -8,47 +8,50 @@
 
 @section('content')
 
-    <div class="row well well-lg">
+    <div class="well well-lg">
 
         @if($user !== false)
-
-            <div class="col-md-12">
-                <img src="{{$user->avatar}}" width="64" height="64"/>
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="{{$user->avatar}}" width="64" height="64"/>
+                </div>
             </div>
 
             <div class="row">
-                @if($user->games->isEmpty())
-                    <div style="text-align: center;">
-                        <h5>No games yet!</h5>
-                    </div>
-                @else
-                    <table class="table">
-                        <tr>
-                            <th></th>
-                            <th>Start Time</th>
-                            <th>End Time</th>
-                            <th>Duration</th>
-                            <th></th>
-                        </tr>
-                        @foreach($user->games as $game)
+                <div class="col-md-12">
+                    @if($user->games->isEmpty())
+                        <div style="text-align: center;">
+                            <h5>No games yet!</h5>
+                        </div>
+                    @else
+                        <table class="table">
                             <tr>
-                                <td><img src="{{$user->avatar}}" width="32" height="32"/></td>
-                                <td>{{$game->time_start}}</td>
-                                <td>{{$game->time_end}}</td>
-
-                                @php
-                                    $start = strtotime($game->time_start);
-                                    $end = strtotime($game->time_end);
-                                @endphp
-                                <td>{{date('H:i:s', ($end - $start))}}</td>
-                                <td><a class="btn btn-raised btn-primary"
-                                       href="aoe2hdspectator://downstream/{{$game->id}}/{{$game->owner->id}}"
-                                       role="button">Watch</a>
-                                </td>
+                                <th></th>
+                                <th>Start Time</th>
+                                <th>End Time</th>
+                                <th>Duration</th>
+                                <th></th>
                             </tr>
-                        @endforeach
-                    </table>
-                @endif
+                            @foreach($user->games as $game)
+                                <tr>
+                                    <td><img src="{{$user->avatar}}" width="32" height="32"/></td>
+                                    <td>{{$game->time_start}}</td>
+                                    <td>{{$game->time_end}}</td>
+
+                                    @php
+                                        $start = strtotime($game->time_start);
+                                        $end = strtotime($game->time_end);
+                                    @endphp
+                                    <td>{{date('H:i:s', ($end - $start))}}</td>
+                                    <td><a class="btn btn-raised btn-primary"
+                                           href="aoe2hdspectator://downstream/{{$game->id}}/{{$game->owner->id}}"
+                                           role="button">Watch</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    @endif
+                </div>
             </div>
 
         @else
